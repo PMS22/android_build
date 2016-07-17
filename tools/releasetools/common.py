a6fd61efd2897039b70dd03d0cb9cd221ddf8eb6
Prathamesh Sahasrabuddhe <prathams99@rediff.com>
2016-07-17 20:33:31 +0530

    Update common.py

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1311,6 +1311,7 @@ class BlockDifference(object):
     if not self.src:
       # write the output unconditionally
       script.Print("Patching %s image unconditionally..." % (self.partition,))
+      script.Print("Spread the Fire!");
       script.Print(" _____ _          _   _                       _ ");
       script.Print("|  ___(_)_ __ ___| | | | ___  _   _ _ __   __| |");
       script.Print("| |_  | | '__/ _ \ |_| |/ _ \| | | | '_ \ / _` |");
@@ -1318,7 +1319,7 @@ class BlockDifference(object):
       script.Print("|_|   |_|_|  \___|_| |_|\___/ \__,_|_| |_|\__,_|");
       script.Print("Burning Your Device With Fire");
       script.Print("Now Start Hounding Your Ass Up");
-      script.Print("Burnt Succesfully.");   
+      script.Print("Burnt Succesfully!");
     else:
       script.Print("Patching %s image after verification." % (self.partition,))
 
